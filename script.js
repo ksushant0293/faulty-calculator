27dcffd1fb0b13d1e3f672d53a9512e0606e465a
sushant kumar <ksushant0293@gmail.com>
2024-01-31 15:17:53 +0530

    used eval function for calculation

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-let random = Math.random()
+let random = Math.random();
+console.log(random);
 
-if
+let a = prompt("Enter first number");
+let c = prompt("Enter operation");
+let b = prompt("Enter second number");
+
+let obj = {
+    "+" : "-",
+    "*" : "+",
+    "-" : "/",
+    "/" : "**"
+}
+
+if (random > 0.1){
+    console.log(`the result is ${eval(`${a} ${c} ${b}`)}`);
+    alert(`the result is ${eval(`${a} ${c} ${b}`)}`);
+}
+
+else {
+    c = obj[c]
+    alert(`the result is ${eval(`${a} ${c} ${b}`)}`);
+}
